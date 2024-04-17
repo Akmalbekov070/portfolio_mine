7d779a578da854bc5fb466e2f3440da389bb78c9
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-04-18 01:10:26 +0300

    News image upload

--- src/components/NewsPage/newsPage.jsx
+++ src/components/NewsPage/newsPage.jsx
@@ -1,10 +1,12 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 import React from 'react';
 
 export default function NewsPage() {
 	return (
-		<Box w={'full'} h={'100vh'} bg={'black'} pt={'100px'} px={24}>
-			<Box>salom</Box>
+		<Box w={'full'} h={'100vh'} bg={'black'} pt={'100px'} display={'flex'} justifyContent={'center'} px={{ base: 2 }}>
+			<Box w={'full'} h={'full'}>
+				<Image src='https://i.postimg.cc/7Yf2HSnt/photo-2024-04-05-00-58-35.jpg' alt='news image' objectFit={'cover'} />
+			</Box>
 		</Box>
 	);
 }
