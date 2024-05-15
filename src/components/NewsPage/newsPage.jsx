e6ed8cc63ce8d3241b0c73cfc68be9093a7aa23c
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-05-15 21:43:44 +0300

    design new page upload new image

--- src/components/NewsPage/newsPage.jsx
+++ src/components/NewsPage/newsPage.jsx
@@ -4,12 +4,25 @@ import React from 'react';
 export default function NewsPage() {
 	return (
 		<>
-			<Box w={'full'} h={'110vh'} bg={'black'} pt={'100px'} display={'flex'} justifyContent={'center'} px={{ base: 2 }}>
+			<Box
+				w={'full'}
+				h={''}
+				bg={'black'}
+				pt={'100px'}
+				display={'flex'}
+				justifyContent={'center'}
+				flexDirection={'column'}
+				alignItems={'center'}
+				px={{ base: 2 }}
+			>
 				<Box>
 					<Text py={4} color={'white'} fontSize={'2xl'}>
 						The news informs that IT Course is opening new courses
 					</Text>
 					<Image src='https://i.postimg.cc/7Yf2HSnt/photo-2024-04-05-00-58-35.jpg' alt='news image' objectFit={'cover'} />
+				</Box>
+				<Box py={10}>
+					<Image src='https://i.postimg.cc/MpNgcvhx/photo-2024-04-23-23-22-29.jpg' alt='new image' objectFit={'cover'} />
 				</Box>
 			</Box>
 		</>
